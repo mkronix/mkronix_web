8cf23fb1295b0252d7035bcb710bc9fd5da22e17
Kasim-bigshorts <Kasim.Kadiwala@bigshorts.co>
2024-11-14 19:22:25 +0530

    testing changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './card.css';
-
-const Card = ({ backgroundImage, title, description, number }) => {
+const Card = ({ backgroundImage, title, description, number = "01" }) => {
     return (
-        <div className='list_inner'>
-            <div className='list_item'>
-                <div className="media_frame">
-                    <img src={backgroundImage} alt={title} className='media' />
-                </div>
-                <div className="content_frame">
-                    <div className='number'>{`[${number}]`}</div>
-                    <div className='card_title'>{title}</div>
-                    <div className='card_text'>{description}</div>
+        <div className={`content content--sticky content--card`}>
+            <div className='list_inner --sticky'>
+                <div className='list_item'>
+                    <div className="media_frame">
+                        <img src={backgroundImage} alt={title} className='media' />
+                    </div>
+                    <div className="content_frame">
+                        <div className='card_title'>{title}</div>
+                        <div className='card_text'>{description}</div>
+                    </div>
                 </div>
             </div>
         </div>
