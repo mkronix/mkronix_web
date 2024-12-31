989de93b2e2a7e31909977bea4be33e463e56718
mkronix <mkronix.official@gmail.com>
2024-12-31 13:40:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AnimatedSideMenu from "./components/AnimatedSideMenu/AnimatedSideMenu";
 import ZeroToHundredLoader from "./components/BestLoader/ZeroToHunderedLoader";
 import useLoading from "./hooks/useLoading";
 import useSmoothScroll from "./hooks/useSmoothScroll";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 const App = () => {
   const loading = useLoading(2000);
   useSmoothScroll(false);
