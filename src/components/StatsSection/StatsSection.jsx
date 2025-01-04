4710b1ce851e6c82d1f9697b5c14e1cc60ecabba
Kasimkkn <kasimkkn15@gmail.com>
2025-01-04 10:40:44 +0530

    added new stuff

--- src/components/StatsSection/StatsSection.jsx
+++ src/components/StatsSection/StatsSection.jsx
@@ -3,18 +3,20 @@ import statsData from '../../data/statssection'
 import GridCardDesign from '../GridCardDesign/GridCardDesign'
 import LampContainer from '../LampContainer/LampContainer'
 import { NeonGradientCard } from '../NeonCard/NeonCard'
+import MagneticButton from '../MagneticButton/MagneticButton'
 const StatsSection = () => {
     return (
         <section className='relative z-10 h-screen w-full flex flex-col my-6'>
             <LampContainer>
                 <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 text-center my-8 text-off-white relative">
                     {statsData.map((stat, index) => (
-                        <NeonGradientCard key={index}>
-                            <div className=''>
-                                <h3 className="text-dynamic-p font-bold text-off-white">{stat.title}+</h3>
-                                <p className="md:text-xl">{stat.description}</p>
-                            </div>
-                        </NeonGradientCard>
+                        <MagneticButton bgColor='bg-transparent' textColor='text-off-white'>
+                            <GridCardDesign
+                                key={index}
+                                title={stat.title}
+                                description={stat.description}
+                            />
+                        </MagneticButton>
                     ))}
                 </div>
             </LampContainer>
